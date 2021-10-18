2db6c20265f4c3743ebdbb02b4f185f5e5798410
Alexander Widua <a.widua@gmail.com>
2021-10-18 12:00:43 +0200

    Add footer

---
+++ js/footer.js
@@ -0,0 +1,12 @@
+/**
+ * @file Footer component that is used across different extension pages (onboarding, options, etc.)
+ */
+
+const footer = document.querySelector('footer')
+footer.innerHTML = `<a href='#'>Intention</a>, made by <a href='#'>Alex Widua</a>.
+    <br/>
+    <ul>
+        <li><a href='#'>View Source on GitHub</a></li>
+        <li>&bull;</li>
+        <li><a href='#'>Privacy Policy</a></li>
+    </ul>`
