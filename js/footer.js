e7d882e8efad66c5b9c1bbefb65a470fb2625fe0
Alexander Widua <a.widua@gmail.com>
2021-10-18 18:42:29 +0200

    update footer links

--- js/footer.js
+++ js/footer.js
@@ -3,10 +3,10 @@
  */
 
 const footer = document.querySelector('footer')
-footer.innerHTML = `<a href='#'>Intention</a>, made by <a href='#'>Alex Widua</a>.
+footer.innerHTML = `<a href='https://github.com/alexwidua/with-intention/'>Intention</a>, made by <a href='https://www.alexwidua.com/'>Alex Widua</a>.
     <br/>
     <ul>
-        <li><a href='#'>View Source on GitHub</a></li>
+        <li><a href='https://github.com/alexwidua/with-intention/'>View Source on GitHub</a></li>
         <li>&bull;</li>
-        <li><a href='#'>Privacy Policy</a></li>
+        <li><a href='https://aluminum-agate-fb1.notion.site/Intentional-Privacy-Policy-b72be50e55e44f378074264c16b1f3a7'>Privacy Policy</a></li>
     </ul>`
